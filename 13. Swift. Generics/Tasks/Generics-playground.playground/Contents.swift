c2a19ccb5943914280c2a53368727af77f4f7980
Egor Badmaev <badmaeve2511@gmail.com>
2021-10-29 11:47:44 +0300

    feat: add solution to the first and second tasks

--- 13. Swift. Generics/Tasks/Generics-playground.playground/Contents.swift
+++ 13. Swift. Generics/Tasks/Generics-playground.playground/Contents.swift
@@ -9,7 +9,7 @@
 //:
 //: Измените функцию так, чтобы она возвращала наименьшее значение из массива `items`.
 
-func lowest<T>(_ items: [T]) -> T? { nil }
+func lowest<T: Comparable>(_ items: [T]) -> T? { items.min() }
 
 let items: [Int] = (0...10).compactMap { (-200...($0 * 100)).randomElement() }
 
@@ -25,19 +25,56 @@ if let value = lowest(items) {
 //: 1. Элементы хранились в контейнере типа `Set`
 //: 2. Метод `dequeue` извлекал минимальный элемент
 
-struct OrderedQueue<T> {
-    
-    var items: [T]
-    
-    mutating func dequeue() -> T? { nil }
-    
+struct OrderedQueue<T: Hashable & Comparable> {
+    var items: Set<T> = []
+    mutating func dequeue() -> T? {
+//        var tmp: [T] = items.sorted(by: {$0 < $1})
+//        return tmp[0]
+        items.min()
+    }
 }
+
+var orderedQueue = OrderedQueue<Int>(items: [5, 1, 2, 3, 4])
+orderedQueue.items
+orderedQueue.dequeue()
+orderedQueue.items
 
 //: ### `associatedtype`
 //:
 //: Произведите рефакторинг кода, применяя знания об `associatedtype`, для классов, описанных ниже.
 //:
+protocol Types {
+    associatedtype ItemType
+    var id: ItemType { get set }
+}
 
+class Passenger: Types {
+    typealias ItemType = String
+    var id: String = ""
+}
+
+class Container: Types {
+    // а тут не пишу тайпэлиас, всё равно всё норм
+    var id: String = ""
+}
+
+class Van: Types {
+    var id: String = ""
+}
+
+class Airbus {
+    var items: [Passenger] = []
+}
+
+class Train {
+    var items: [Van] = []
+}
+
+class Seaship {
+    var items: [Container] = []
+}
+
+/* orig
 class Passenger {
     var id: String = ""
 }
@@ -61,8 +98,7 @@ class Train {
 class Seaship {
     var items: [Container] = []
 }
-
-
+*/
 //: ### `where`
 //:
 //: Для типа `OrderedQueue`, описанного выше, добавьте расширения с условиями:
@@ -71,7 +107,9 @@ class Seaship {
 //: 2. Если тип `T` представлен строковым типом, в интерфейсе доступен метод `prefix`, возвращающий наибольший общий префикс у всех элементов
 
 extension OrderedQueue {
-    
+    where T == Int {
+    var count: Int = 0
+    }
 }
 
 extension OrderedQueue {
